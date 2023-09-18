1875325ad381a4c9ce8562693cda6378f7c08a62
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-09-18 09:15:26 -0500

    Update url.py
    changed the docstring a little bit

--- src/DisOAuth/url.py
+++ src/DisOAuth/url.py
@@ -15,19 +15,18 @@ class AuthUrl:
         """Makes and returns a url that is used to authorize users
 
         Keyword arguments:
-        client_id -- The client ID of your discord app
-        scope -- A list of scopes you want to use
-        redirect_uri -- The redirect uri you want to use
+        client_id: The client ID of your discord app
+        scope: A list of scopes you want to use
+        redirect_uri: The redirect uri you want to use
         """
         self._bUrl = "https://discord.com/oauth2/authorize?response_type=code&"
         self._client_id = "client_id=" + client_id + "&"
-        _x = 0
         _strScope = ""
         while _x < len(scope):
             _strScope = _strScope + scope[_x] + "%20"
         _x += 1
         self._scope = "scope=" + _strScope[:-3]
-        _redRep1 = redirect_uri.replace(":", "%3A")
+        _redRep1 =g redirect_uri.replace(":", "%3A")
         _redRep2 = _redRep1.replace("/", "%2F")
         _redRep3 = _redRep2.replace("-", "%2D")
         self._redirect_uri = "redirect_uri=" + _redRep3
@@ -53,10 +52,10 @@ class discordApi:
                  redirect_uri) -> None:
         """Where you can get an access code, use the api links to get info
 
-        client_id - The client id of your application
-        client_secret - The client secret of your application
-        scope - a list of the scopes you authorized for
-        redirect_uri -- the redirect_uri you want to use
+        client_id: The client id of your application
+        client_secret: The client secret of your application
+        scope: a list of the scopes you authorized for
+        redirect_uri: the redirect_uri you want to use
         """
         self.client_id = client_id
         self.client_secret = client_secret
